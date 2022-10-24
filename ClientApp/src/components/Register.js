308acc68a8b12bd4e48c61d5e27121070516325d
INT\Victor.Rosario <victor.rosario@solera.com>
2022-10-24 15:28:49 +0200

    Register

--- ClientApp/src/components/Register.js
+++ ClientApp/src/components/Register.js
@@ -1,9 +1,46 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-export class Register extends Component{
-  render(){
-  return (
-    <div>Registers</div>
-  )
-}
+export class Register extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+                        <h2 className='text-center m-4'> Register</h2>
+                        <div className='mb-3'>
+                            <label htmlFor="Nombre" className='form-label'>
+                                Nombre
+                            </label>
+                            <input type={"text"} className="form-control" placeholder='Escriba su nombre' name='nobre' />
+                        </div>
+                        <div className='mb-3'>
+                            <label htmlFor="Apellido" className='form-label'>
+                                Apellido:
+                            </label>
+                            <input type={"text"} className="form-control" placeholder='Escriba su apellido' name='apellido' />
+                        </div>
+                        <div className='mb-3'>
+                            <label htmlFor="Email" className='form-label'>
+                                Email:
+                            </label>
+                            <input type={"email"} className="form-control" placeholder='Escriba su correo' name='email' />
+                        </div>
+
+
+
+                        <div className='mb-3'>
+                            <label htmlFor="Contraseña" className='form-label'>
+                                Contraseña
+                            </label>
+                            <input type={"password"} className="form-control" placeholder='Escriba su contraseña' name='contraseña' />
+
+                        </div>
+                        <button type='submit' className='btn btn-outline-primary'>Enviar</button>
+                        <Link type='submit' className='btn btn-outline-danger mx-2' to="/">Cancelar</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
